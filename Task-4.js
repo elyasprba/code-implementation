791ac3d45c739c124b18ebd6fb810c13e004c115
elyasprba <elyaspoerba21@gmail.com>
2022-04-20 10:14:08 +0700

    revisi

--- Task-4.js
+++ Task-4.js
@@ -4,8 +4,11 @@
  * Output : 55566914566956
  */
 
+// num dijadikan string dan displit
+// split merubah menjadi array, terus method map meloop tiap karakter di array, terus dimasukin ke partResult
+// habis selesai looping, part result di sort lalu di join, lalu mengembalikan nilai part result ke array yang di map tadi
+
 function devideAndSort(num) {
-   // num dijadikan string dan displit
    let hasil = num
       .toString()
       .split(0)
@@ -18,8 +21,9 @@ function devideAndSort(num) {
          return partResult;
       });
    // Mangubah string menjadi number
-   hasil = hasil.join('') * 1;
-   return console.log(hasil);
+   let result = hasil.join(''); // Tambahkan (* 1)
+   return console.log(Number(result));
+   // return console.log(result);
 }
 
 devideAndSort(98706540321);
