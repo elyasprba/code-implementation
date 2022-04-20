04ec98cd5f482899c7310cabf27e8e5bc507cb5a
elyasprba <elyaspoerba21@gmail.com>
2022-04-20 09:05:20 +0700

    init

--- Task-2.js
+++ Task-2.js
@@ -4,19 +4,11 @@
  * Misalnya kalimat "Saya Belajar Javascript" output : "Javascipt belajar Saya"
  */
 
-function reverseWords(key) {
-   let result = '';
-   let newResult = '';
-   for (let i = 0; i <= key.length - 1; i++) {
-      if (key[i] !== ' ') {
-         newResult += key[i];
-      } else {
-         result = newResult + ' ' + result;
-         newResult = '';
-      }
-   }
-   result = newResult + ' ' + result;
-   return console.log(result);
+function reverseWords(sentence) {
+   sentence = sentence.split(' ');
+   let result = [];
+   result = sentence.reverse();
+   return console.log(result.join(' '));
 }
 
 reverseWords('Saya belejar Javascript');
